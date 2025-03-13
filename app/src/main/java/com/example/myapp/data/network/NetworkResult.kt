199cebe58d5aed7d1b65ec8da54c5ab9164bdf4b
Nguyen Anh Duc <nguyenanhduchd@gmail.com>
2025-03-13 14:12:52 +0700

    Add app/src/main/java/com/example/myapp/data/network/NetworkResult.kt 1741849971367

---
+++ app/src/main/java/com/example/myapp/data/network/NetworkResult.kt
@@ -0,0 +1,13 @@
+package com.example.myapp.data.network
+
+sealed class NetworkResult<T> {
+    data class Success<T>(val data: T) : NetworkResult<T>()
+    data class Error<T>(val message: String, val code: Int? = null) : NetworkResult<T>()
+    class Loading<T> : NetworkResult<T>()
+
+    companion object {
+        fun <T> loading() = Loading<T>()
+        fun <T> success(data: T) = Success(data)
+        fun <T> error(message: String, code: Int? = null) = Error<T>(message, code)
+    }
+}
